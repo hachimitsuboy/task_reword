52d8f0189b9dfc47a243146f7a2bcbd538da9c2f
hachimitsuboy <hachimitsu8897@icloud.com@example.com>
2022-09-19 12:22:39 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/task_reword/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.task_reword
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:task_reword/view/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
